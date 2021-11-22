d0f362e3710e95acd21c986d958be87cc172e61d
Fritza02 <fritza.v@hotmail.com>
2021-11-22 11:27:13 -0300

    funcion eliminar funcionando

--- src/views/login.js
+++ src/views/login.js
@@ -1,4 +1,5 @@
 import { loginUser, inGoogle } from '../lib/firebase.js';
+import { registerPage } from './register.js';
 
 export const loginPage = () => {
   const containerRoot = document.getElementById('root');
@@ -8,19 +9,31 @@ export const loginPage = () => {
     <div class='logo'>
         <img alt='logo login' src='./images/logo1.png' class = 'logotypeLogin'>   
     </div>
-    <div class= 'loginForm'>
+    <div class= 'sectionLogin'>
+      <div class= 'loginForm'>
         <input type='email' id='loginEmail' class='loginEmail' placeholder='Correo electrónico'>
         <input type='password'id='loginPassword' class='loginPassword' placeholder='Contraseña'>
         <button  class='btnLogin' id='btnLogin'> Iniciar sesión</button>
-    </div>
-    <div class='loginOption'>
+      </div>
+      <div class='loginOption'>
         <p>Iniciar sesión con</p>
         <img src='./images/google.png' id='googleLogo' class='googleLogo'>
-        <div class='userReg'> ¿No tienes cuenta? <a href='#/register' id='userReg' class='userReg'> <strong>Registrate</strong></a></div>
+        
+        
+        <div class='userReg'>
+        ¿No tienes cuenta?  
+         <label for="btn-moda" class="lbl-moda">
+         <strong>Registrate</strong>  
+         </label>
+         </div>
+      
+        
+      </div>
     </div>
     `;
   loginSection.innerHTML = login;
   containerRoot.appendChild(loginSection);
+  containerRoot.appendChild(registerPage());
   document.getElementById('btnLogin').addEventListener('click', () => {
     const logEmail = document.getElementById('loginEmail').value;
     const logPass = document.getElementById('loginPassword').value;
